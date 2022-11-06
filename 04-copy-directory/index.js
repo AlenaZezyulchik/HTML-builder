7bd27e5557e6198f6ab003272ba40d963e9452ab
Alena Zezyulchik <alen4ik_6216@mail.ru>
2022-11-06 13:17:56 +0300

    feat: completed task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+let fs = require('fs/promises');
+let path = require('path');
 
+let dir = path.join(__dirname, 'files');
+let copy = path.join(__dirname, 'files-copy');
+
+fs.rm(copy, {recursive: true, force: true}).finally(function() {
+  fs.mkdir(copy, {recursive: true});
+  fs.readdir(dir, {withFileTypes: true}).then(function(elems) {
+    elems.forEach(function(elem) {
+      if (elem.isFile()) {
+        let item = path.join(dir, elem.name);
+        let itemDes = path.join(copy, elem.name);
+        fs.copyFile(item, itemDes);}
+        });
+    });
+});
